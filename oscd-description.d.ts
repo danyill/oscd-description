e74d1d7e2034d75d7287d3142112150ea7fb5fb5
danyill <danyill@users.noreply.github.com>
2023-05-04 20:48:01 +0000

    Deploying to gh-pages from @ danyill/oscd-description@3e6a5b953f715a86038c15de0ae8f3362c38efde 🚀

---
+++ oscd-description.d.ts
@@ -0,0 +1,30 @@
+import { LitElement, PropertyValues, TemplateResult } from 'lit';
+import '@material/mwc-button';
+import '@material/mwc-formfield';
+import '@material/mwc-textfield';
+import '@material/mwc-list';
+import '@material/mwc-list/mwc-list-item';
+import '@material/mwc-list/mwc-radio-list-item';
+import '@material/mwc-icon-button-toggle';
+import '@material/mwc-icon';
+import '@material/mwc-icon-button';
+import './foundation/components/oscd-filter-button.js';
+export declare function getFcdaSubtitleValue(fcdaElement: Element): string;
+/**
+ * Editor for GOOSE and SMV supervision LNs
+ */
+export default class Supervision extends LitElement {
+    doc: XMLDocument;
+    docName: string;
+    editCount: number;
+    controlType: 'GOOSE' | 'SMV';
+    private get iedList();
+    selectedIEDs: string[];
+    private get selectedIed();
+    protected firstUpdated(): void;
+    protected updated(_changedProperties: PropertyValues): void;
+    private renderIedSelector;
+    protected render(): TemplateResult;
+    onFilterInput: (...args: any) => void;
+    static styles: import("lit").CSSResult;
+}
